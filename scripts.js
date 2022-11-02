4eb819807a6519af4c6f9e56af9efbf4f09abba9
Y3k <carlo.arnaboldi@outlook.com>
2022-11-02 02:53:39 +0100

    ultime correzioni

---
+++ scripts.js
@@ -0,0 +1,13 @@
+document.addEventListener('click', function (event) {
+
+	// If the clicked element doesn't have the right selector, bail
+	if (!event.target.matches('#show-more-btn') && !event.target.matches('#show-more-btn > span')) return;
+
+    var elements = document.getElementsByClassName('initially-hidden');
+
+    for(var el of elements){
+        el.classList.toggle('fade');
+    }
+
+    document.getElementById('show-more-btn').classList.add('initially-hidden');
+});
